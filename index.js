baf8f3b50379d358f08ac3d94ce7afc4912cbd12
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2023-01-24 19:13:13 +0600

    added file

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ii6xxd.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database Connected')
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello From Txdap!')
+})
+
+app.listen(port, () => {
+    console.log(`Txdap App listening on port ${port}`)
+})
